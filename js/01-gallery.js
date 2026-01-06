371b5d301425a521c11b750ecea406fd37db8837
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2026-01-06 14:57:51 +0200

    task 01 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const gallerylist = document.querySelector(".gallery");
+gallerylist.addEventListener("click", onClick);
 
-console.log(galleryItems);
+let instance = null;
+
+const galleryListItem = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallerylist.insertAdjacentHTML("beforeend", galleryListItem);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const { target } = evt;
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+  showModal(target);
+}
+
+function showModal({ dataset: { source }, alt }) {
+  instance = basicLightbox.create(
+    `
+    <img src=${source} alt=${alt}>
+`,
+    {
+      onShow: () => {
+        document.addEventListener("keydown", onKeyDown);
+      },
+
+      onClose: () => {
+        document.removeEventListener("keydown", onKeyDown);
+      },
+    }
+  );
+
+  instance.show();
+}
+
+function onKeyDown(evt) {
+  if (evt.code === "Escape" && instance.visible()) {
+    instance.close();
+  }
+}
